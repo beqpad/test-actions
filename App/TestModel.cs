0bb4876c8bc48e8f2d79ea0aa548a0b45c4db0f2
beqpad <beqpad@hotmail.com>
2022-07-13 18:43:15 +0900

    Update TestModel.cs

--- App/TestModel.cs
+++ App/TestModel.cs
@@ -3,4 +3,12 @@ namespace test_actions;
 public class TestModel
 {
     public string GetValue() => "abcde";
+    
+    public string? String1 { get; init; }
+    
+    public int GetLength() => String1.Length;
+    
+    public Action<string> Predicate { get; set; }
+    
+    public void FireAction(string? message) => Predicate(message);
 }
